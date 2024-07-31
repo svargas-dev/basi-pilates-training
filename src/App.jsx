78cad5f9cab22c626fc33831e97cc9523b167743
Sam Vargas <sam.vargas@gmx.com>
2024-07-31 23:29:10 +0100

    Try fetching live data...

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,168 @@
-import { useEffect, useState } from "react";
-import allData from "./assets/data.json";
+import { useEffect, useMemo, useState } from "react";
 
 import "./App.css";
 
-const data = allData["0"];
-
-const courses = data
-  .reduce((acc, datum) => {
-    if (!acc.includes(datum.program_name)) {
-      acc.push(datum.program_name);
-    }
-    return acc;
-  }, [])
-  .sort();
-
-const countries = data
-  .reduce((acc, datum) => {
-    if (!acc.includes(datum.studio_country)) {
-      acc.push(datum.studio_country);
-    }
-    return acc;
-  }, [])
-  .sort();
-
 function App() {
-  const [filteredData, setFilteredData] = useState(data);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  const [filteredData, setFilteredData] = useState([]);
   const [activeCourses, setActiveCourses] = useState([]);
   const [activeCountries, setActiveCountries] = useState([]);
 
+  const courses = useMemo(
+    () =>
+      data
+        ?.reduce((acc, datum) => {
+          if (!acc.includes(datum.program_name)) {
+            acc.push(datum.program_name);
+          }
+          return acc;
+        }, [])
+        .sort() ?? [],
+    [data],
+  );
+
+  const countries = useMemo(
+    () =>
+      data
+        ?.reduce((acc, datum) => {
+          if (!acc.includes(datum.studio_country)) {
+            acc.push(datum.studio_country);
+          }
+          return acc;
+        }, [])
+        .sort() ?? [],
+    [data],
+  );
+
   useEffect(() => {
-    if (activeCourses.length === 0) {
-      setFilteredData(
-        data.filter((datum) => activeCountries.includes(datum.studio_country)),
-      );
+    if (data) {
+      if (activeCourses.length === 0) {
+        setFilteredData(
+          data.filter((datum) =>
+            activeCountries.includes(datum.studio_country),
+          ),
+        );
+      }
+      if (activeCountries.length === 0) {
+        setFilteredData(
+          data
+            .filter((datum) => activeCourses.includes(datum.program_name))
+            .sort((a, b) => (a.studio_country < b.studio_country ? -1 : 1)),
+        );
+      }
+      if (activeCourses.length > 0 && activeCountries.length > 0) {
+        setFilteredData(
+          data
+            .filter(
+              (datum) =>
+                activeCourses.includes(datum.program_name) &&
+                activeCountries.includes(datum.studio_country),
+            )
+            .sort((a, b) => (a.studio_country < b.studio_country ? -1 : 1)),
+        );
+      }
     }
-    if (activeCountries.length === 0) {
-      setFilteredData(
-        data
-          .filter((datum) => activeCourses.includes(datum.program_name))
-          .sort((a, b) => (a.studio_country < b.studio_country ? -1 : 1)),
-      );
-    }
-    if (activeCourses.length > 0 && activeCountries.length > 0) {
-      setFilteredData(
-        data
-          .filter(
-            (datum) =>
-              activeCourses.includes(datum.program_name) &&
-              activeCountries.includes(datum.studio_country),
-          )
-          .sort((a, b) => (a.studio_country < b.studio_country ? -1 : 1)),
-      );
-    }
-  }, [activeCountries, activeCourses]);
+  }, [data, activeCountries, activeCourses]);
+
+  useEffect(() => {
+    fetch("https://www.basipilates.com/wp-json/api/search/get/courses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: "data[0][name]=program&data[0][value]=Teacher+Training",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setError({ message: "😿" });
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setData(data[0]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
-      <h1>BASI Pilates Teacher Training Courses</h1>
+      <h1>BASI Pilates</h1>
+      <h2>Teacher Training Courses</h2>
 
-      <p>
-        This is an unofficial directory of BASI teacher training studios. This
-        may be outdated. Data retrieved 26 July 2024.
-      </p>
-      <h2>Filter by course</h2>
-      <div>
-        {courses.map((course) => (
-          <button
-            key={course}
-            className={activeCourses.includes(course) ? "active" : "inactive"}
-            onClick={() =>
-              setActiveCourses(
-                activeCourses.includes(course)
-                  ? activeCourses.filter((c) => c !== course)
-                  : [...activeCourses, course],
-              )
-            }
-          >
-            {course}
-          </button>
-        ))}
-      </div>
+      {!data && !error && <p>Loading...</p>}
+      {!!error && <p>Fail: {error.message}</p>}
 
-      <h2>Filter by country</h2>
-      <div>
-        {countries.map((country) => (
-          <button
-            key={country}
-            className={
-              activeCountries.includes(country) ? "active" : "inactive"
-            }
-            onClick={() =>
-              setActiveCountries(
-                activeCountries.includes(country)
-                  ? activeCountries.filter((c) => c !== country)
-                  : [...activeCountries, country],
-              )
-            }
-          >
-            {country}
-          </button>
-        ))}
-      </div>
-      <h2>Number of studios: {filteredData.length}</h2>
+      {!!data && data.length > 0 && (
+        <>
+          <h3>Filter by course</h3>
+          <div>
+            {courses.length > 0 &&
+              courses.map((course) => (
+                <button
+                  key={toKebabCase(course)}
+                  className={
+                    activeCourses.includes(course) ? "active" : "inactive"
+                  }
+                  onClick={() =>
+                    setActiveCourses(
+                      activeCourses.includes(course)
+                        ? activeCourses.filter((c) => c !== course)
+                        : [...activeCourses, course],
+                    )
+                  }
+                >
+                  {course}
+                </button>
+              ))}
+          </div>
 
-      {filteredData.map((datum) => (
-        <div key={datum.course_id} className="card">
-          <h3>{datum.studio_name}</h3>
-          <p>
-            <em>{datum.program_name}</em>
-          </p>
-          <p>{datum.studio_city}</p>
-          <p>{datum.studio_country}</p>
-          <details>
-            <summary>See all info</summary>
-            <code>
-              <pre>{JSON.stringify(cleanDatum(datum), null, 4)}</pre>
-            </code>
-          </details>
-        </div>
-      ))}
+          <h3>Filter by country</h3>
+          <div>
+            {countries.length &&
+              countries.map((country) => (
+                <button
+                  key={toKebabCase(country)}
+                  className={
+                    activeCountries.includes(country) ? "active" : "inactive"
+                  }
+                  onClick={() =>
+                    setActiveCountries(
+                      activeCountries.includes(country)
+                        ? activeCountries.filter((c) => c !== country)
+                        : [...activeCountries, country],
+                    )
+                  }
+                >
+                  {country}
+                </button>
+              ))}
+          </div>
+          <h2>Number of studios: {filteredData?.length ?? 0}</h2>
+
+          {filteredData.length > 0 &&
+            filteredData.map((datum) => (
+              <div key={datum.course_id} className="card">
+                <h3>{datum.studio_name}</h3>
+                <p>
+                  <em>{datum.program_name}</em>
+                </p>
+                <p>{datum.studio_city}</p>
+                <p>{datum.studio_country}</p>
+                <details>
+                  <summary>See all info</summary>
+                  <code>
+                    <pre>{JSON.stringify(cleanDatum(datum), null, 4)}</pre>
+                  </code>
+                </details>
+              </div>
+            ))}
+        </>
+      )}
     </>
   );
 }
@@ -164,5 +208,13 @@ const cleanDatum = (datum) => {
 
   return datum;
 };
+
+function toKebabCase(str) {
+  return str
+    .replace(/[^a-zA-Z0-9]/g, "-")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase()
+    .replace(/--+/g, "-");
+}
 
 export default App;
